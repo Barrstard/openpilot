237350c8db1d8459e5ea26448ac6ce391ab5acd6
Barrstard <Guy.barr@outlook.com>
2020-07-31 09:44:18 +1000

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -14,12 +14,12 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 70.  # one minute limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
+_AWARENESS_TIME = 700.  # one minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 150.  # a first alert is issued 25s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_DISTRACTED_TIME = 110.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 80.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 60.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
@@ -36,8 +36,8 @@ _PITCH_POS_ALLOWANCE = 0.12  # rad, to not be too sensitive on positive pitch
 _PITCH_NATURAL_OFFSET = 0.02  # people don't seem to look straight when they drive relaxed, rather a bit up
 _YAW_NATURAL_OFFSET = 0.08  # people don't seem to look straight when they drive relaxed, rather a bit to the right (center of car)
 
-_HI_STD_TIMEOUT = 50
-_HI_STD_FALLBACK_TIME = 100  # fall back to wheel touch if model is uncertain for a long time
+_HI_STD_TIMEOUT = 5000
+_HI_STD_FALLBACK_TIME = 10000  # fall back to wheel touch if model is uncertain for a long time
 _DISTRACTED_FILTER_TS = 0.25  # 0.6Hz
 
 _POSE_CALIB_MIN_SPEED = 13  # 30 mph
